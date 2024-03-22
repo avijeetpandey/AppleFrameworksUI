83978d34bab01a9a8d0e1ed7964614edf84f94db
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-22 21:16:44 +0530

    Add: initial commit

---
+++ AppleFrameworksUI/AppleFrameworksUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksUIApp.swift
+//  AppleFrameworksUI
+//
+//  Created by Avijeet on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
